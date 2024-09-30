62121a0e43aa37285e0d65847f7156e80dcd4b38
Jinhong <jinholee@student.42seoul.kr>
2024-09-30 16:55:34 +0200

    merged io_handlers and added queue feature

--- src/io_handlers.c
+++ src/io_handlers.c
@@ -6,6 +6,60 @@
 #include <unistd.h>
 #include <sys/iofunc.h>
 #include <sys/dispatch.h>
+#include "my_queue.h"
+
+// Global circular queue
+static Queue Q;
+
+int my_read_handler(resmgr_context_t *ctp, io_read_t *msg, RESMGR_OCB_T *ocb)
+{
+	size_t	nbytes;
+	int		nparts;
+	int		status;
+	int		buffer;
+
+	status = iofunc_read_verify(ctp, msg, ocb, NULL);
+	if (status != EOK)
+		return (status);
+
+	// if not _IO_XTYPE_NONE, it's not implemented
+	if ((msg->i.xtype & _IO_XTYPE_MASK) != _IO_XTYPE_NONE)
+		return (ENOSYS);
+
+	// No data, try again
+	if (isEmpty(&Q))
+		return (EAGAIN);
+
+	nbytes = min(_IO_READ_GET_NBYTES(msg), sizeof(int));
+
+	if (nbytes > 0)
+	{
+		// get value from the queue and add 2
+		deQueue(&Q, &buffer);
+		buffer += 2;
+
+		// set up the return data IOV
+		SETIOV(ctp->iov, &buffer, nbytes);
+
+		// set up the number of bytes (returned by clients read())
+		_IO_SET_READ_NBYTES(ctp, nbytes);
+
+		nparts = 1;
+	}
+	else
+	{
+		// zero byte read or already read everything
+		_IO_SET_READ_NBYTES(ctp, 0);
+
+		nparts = 0;
+	}
+
+	// update access time
+	if (msg->i.nbytes > 0)
+		ocb->attr->flags |= IOFUNC_ATTR_ATIME;
+
+	return (_RESMGR_NPARTS(nparts));
+}
 
 int my_write_handler(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb)
 {
@@ -20,6 +74,10 @@ int my_write_handler(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb)
 	// if not _IO_XTYPE_NONE, it's not implemented
 	if ((msg->i.xtype & _IO_XTYPE_MASK) != _IO_XTYPE_NONE)
 		return (ENOSYS);
+
+	// queue is full, would block
+	if (isFull(&Q))
+		return (EWOULDBLOCK);
 
 	// extract the length of the client's message
 	nbytes = _IO_WRITE_GET_NBYTES(msg);
@@ -46,6 +104,7 @@ int my_write_handler(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb)
 		buffer &= mask;
 	}
 	printf("Received bytes: %zu, value: %i\n", nbytes, buffer);
+	enQueue(&Q, buffer);
 
 	// update modified, created time
 	if (nbytes > 0)
@@ -53,4 +112,3 @@ int my_write_handler(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb)
 
 	return (_RESMGR_NPARTS(0));
 }
-
